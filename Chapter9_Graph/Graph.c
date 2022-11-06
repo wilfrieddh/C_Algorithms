ef3670c2e871a2a069a1032723c13cb7c140108b
franneck94 <schaffranek.jan@web.de>
2022-11-06 14:42:52 +0100

    up

--- Chapter9_Graph/Graph.c
+++ Chapter9_Graph/Graph.c
@@ -4,32 +4,39 @@
 
 #include "Graph.h"
 
-graph_t *createGraph(uint32_t num_verticies, uint32_t num_edges)
+graph_t *createGraph(const uint32_t num_verticies, const uint32_t num_edges)
 {
+
 }
 
 graph_t *freeGraph(graph_t *graph)
 {
+
 }
 
-node_t *createNode(uint32_t node_idx,
-                   value_type_t weight,
-                   node_t *previous_node)
+node_t *createNode(const uint32_t node_idx,
+                   const value_type_t weight,
+                   node_t *const previous_node)
 {
+
 }
 
 node_t *freeNode(node_t *node)
 {
+
 }
 
-void addEdges(graph_t *graph, edge_t edges[])
+void addEdges(const graph_t *graph, const edge_t edges[])
 {
+
 }
 
-void removeEdges(graph_t *graph)
+void removeEdges(const graph_t *graph)
 {
+
 }
 
-void printGraph(graph_t *graph)
+void printGraph(const graph_t *const graph)
 {
+
 }
