425987da2426eec105608be6a1fad97854fbf27d
franneck94 <schaffranek.jan@web.de>
2022-11-10 07:23:19 +0100

    up

--- Chapter6_HashTable/HashTable.c
+++ Chapter6_HashTable/HashTable.c
@@ -13,38 +13,54 @@ hash_table_t *createHashTable(void)
         return NULL;
     }
 
+    const size_t data_size = TABLE_SIZE * sizeof(item_t);
+    item_t *data = (item_t *)malloc(data_size);
+
+    if (NULL == data)
+    {
+        free(hash_table);
+
+        return NULL;
+    }
+
+    for (uint32_t i = 0; i < TABLE_SIZE; i++)
+    {
+        strncpy(data[i].key, DELETED_KEY, DELETED_KEY_SIZE);
+        data[i].value = NO_VALUE;
+    }
+
+    hash_table->size = 0u;
+    hash_table->capacity = TABLE_SIZE;
+    hash_table->data = data;
+
     return hash_table;
 }
 
 hash_table_t *freeHashTable(hash_table_t *hash_table)
 {
-    if (NULL == hash_table)
+    if (NULL != hash_table)
     {
-        return NULL;
-    }
+        if (NULL != hash_table->data)
+        {
+            free(hash_table->data);
+        }
 
-    free(hash_table);
+        free(hash_table);
+    }
 
     return NULL;
-}
-
-void printHashTable(const hash_table_t *const hash_table)
-{
-    if (NULL == hash_table)
-    {
-        return;
-    }
 }
 
 uint32_t hash(const char key[MAX_NAME_SIZE])
 {
     uint32_t hash_value = 0u;
 
-    for (uint32_t i = 0u; i < MAX_NAME_SIZE; i++)
+    for (uint32_t i = 0; i < MAX_NAME_SIZE; i++)
     {
         if ('\0' != key[i])
         {
-            hash_value = (hash_value * 128u + key[i]) % TABLE_SIZE;
+            hash_value = ((uint32_t)(hash_value * 128U) + (uint32_t)(key[i])) %
+                         TABLE_SIZE;
         }
         else
         {
@@ -53,4 +69,22 @@ uint32_t hash(const char key[MAX_NAME_SIZE])
     }
 
     return hash_value;
+}
+
+void printHashTable(const hash_table_t *const hash_table)
+{
+    if (NULL == hash_table)
+    {
+        return;
+    }
+
+    printf("\nHashTable contains %u elements with a capcity of %u.\n",
+           hash_table->size,
+           hash_table->capacity);
+
+    for (uint32_t i = 0u; i < TABLE_SIZE; i++)
+    {
+        const item_t *const item = &hash_table->data[i];
+        printf("Index: %u, Key: %s, Value %f\n", i, item->key, item->value);
+    }
 }
